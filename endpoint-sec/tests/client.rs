752163f15e8ba38449afc742ab2eb9c3cbea43f7
roblabla <unfiltered@roblab.la>
2023-11-29 11:11:26 +0100

    Add failing test showing the double drop

--- endpoint-sec/tests/client.rs
+++ endpoint-sec/tests/client.rs
@@ -7,3 +7,28 @@ fn test_client_can_connect() {
     })
     .unwrap();
 }
+
+#[test]
+fn reproduce_double_drop() {
+    #[derive(Debug)]
+    struct DropCnt {
+        // To avoid segfaulting in case we regress, we leak the allocation here.
+        cnt: std::mem::ManuallyDrop<Box<u32>>,
+    }
+    impl Drop for DropCnt {
+        fn drop(&mut self) {
+            println!("Dropping, counter at {}", **self.cnt);
+            **self.cnt += 1;
+            if **self.cnt > 1 {
+                panic!("Dropped more than once");
+            }
+        }
+    }
+    let drop_cnt = DropCnt {
+        cnt: std::mem::ManuallyDrop::new(Box::new(0)),
+    };
+    Client::new(move |_client, _msg| {
+        println!("{:?}", drop_cnt);
+    })
+    .unwrap();
+}
