ad8717a47d98614127e1b9190c63d8c7c39f08e5
Alexis (Poliorcetics) Bourget <ab_github@poliorcetiq.eu>
2023-12-07 10:40:55 +0100

    feat(macos 14): `EventOdCreateUser`

--- endpoint-sec/src/event.rs
+++ endpoint-sec/src/event.rs
@@ -367,6 +367,8 @@ define_event_enum!(
         ES_EVENT_TYPE_NOTIFY_OD_ATTRIBUTE_VALUE_REMOVE => NotifyOdAttributeValueRemove (EventOdAttributeValueRemove [_ => None] { raw: raw_event.od_attribute_value_remove.as_opt()?, version, }),
         /// Notification about an attribute that was set.
         ES_EVENT_TYPE_NOTIFY_OD_ATTRIBUTE_SET => NotifyOdAttributeSet (EventOdAttributeSet [_ => None] { raw: raw_event.od_attribute_set.as_opt()?, version, }),
+        /// Notification about an account that was created.
+        ES_EVENT_TYPE_NOTIFY_OD_CREATE_USER => NotifyOdCreateUser (EventOdCreateUser [_ => None] { raw: raw_event.od_create_user.as_opt()?, version, }),
     }
 );
 
@@ -618,4 +620,5 @@ cfg_mod! {
     mod event_od_attribute_value_add;
     mod event_od_attribute_value_remove;
     mod event_od_attribute_set;
+    mod event_od_create_user;
 }
